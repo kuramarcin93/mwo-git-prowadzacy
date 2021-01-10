f8d0be477e86980a9e72cb7af097a1e446abad97
Marcin Kura <mkura@student.agh.edu.pl>
2021-01-10 15:18:20 +0100

    Further

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,8 +3,5 @@ public class SomeProgram {
       System.out.println("We are learning to use Git."); 
 	  System.out.println("We will be Git experts, soon.");
     }
-	
-	public void manageTheRockets() {
-    // TODO implement
 }
 }
