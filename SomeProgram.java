4ab04e03443c9ce6af1aca2cfb07cba9746ea818
kuramarcin93 <77232817+kuramarcin93@users.noreply.github.com>
2021-01-10 15:37:00 +0100

    Update SomeProgram.java

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,13 +2,6 @@ public class SomeProgram {
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
 	  System.out.println("We will be Git experts, soon.");
+	    System.out.println("Change");
     }
-	
-	public void manageTheRockets() {
-		// TODO implement
-	}
-	
-	public void manageTheSatellite() {
-		// TODO implement
-	}
 }
